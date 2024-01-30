f5596e90b640441b48d6b1ce7ce42031f6e27968
Zunickx <Zunickx@gmail.com>
2024-01-30 22:43:09 +0300

    Create files index.html, index.js, app.js, tooltip.js, style.css, actionTooltip.test.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import "./css/style.css";
+import "./js/app";
+
+// TODO: write your code in app.js
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Tooltip } from "./tooltip";
+const button = document.querySelector(".btn");
+const tooltip = new Tooltip();
+button.addEventListener("click", () => {
+  if (document.querySelector(".tooltip-wrapper") === null) {
+    tooltip.addTooltip(button);
+  } else {
+    tooltip.removeTooltip();
+  }
+});
