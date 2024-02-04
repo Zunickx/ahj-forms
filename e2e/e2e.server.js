cf25c0348dd17301b00c82e10c47b19be9873f80
Zunickx <Zunickx@gmail.com>
2024-02-04 15:10:27 +0300

    Change gitignore added node_modules

---
+++ e2e/e2e.server.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const config = require("../webpack.dev");
+
+const server = new WebpackDevServer(webpack(config), {});
+server.listen(9001, "localhost", (err) => {
+  if (err) {
+    return;
+  }
+  if (process.send) {
+    process.send("ok");
+  }
+});
